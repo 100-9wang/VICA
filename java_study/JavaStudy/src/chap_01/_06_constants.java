a4e9419af5007cb6bc30896d7acd8d9cfc89bd8b
백광민 <carpediem2001@nate.com>
2024-09-03 18:23:01 +0900

    study : 자바 독학 -1일차 1. 자료형 2. 변수 3. 변수 이름 규칙 4. 상수 5. 형변환

---
+++ java_study/JavaStudy/src/chap_01/_06_constants.java
@@ -0,0 +1,16 @@
+package chap_01;
+
+public class _06_constants {
+    public static void main(String[] args) {
+        final String KR_COUNTRY_CODE = "+82"; // 국가 번호 (빨리)
+        // KR_COUNTRY_CODE = "+8282";
+        System.out.println(KR_COUNTRY_CODE);
+
+        final double PI = 3.141592; //원주율
+        //PI = 3.153828;
+        System.out.println(PI);
+
+        final String DATE_OF_BIRTH = "2001-11-10"; // 생년 월일
+
+    }
+}
